988c8a7575c49e62537bf6ec94e88bf68c714c19
Waterfall1092 <takizawa.tokuji@cataracts.onmicrosoft.com>
2025-07-28 13:04:21 +0900

    Update CTF_V_1.js

--- Custom/patterns/CTF_V_1.js
+++ Custom/patterns/CTF_V_1.js
@@ -15,7 +15,7 @@ var CTF_V_1 = {
 				"numBars": 500,
                 "childGeometry": {
 					"primitive": "plane",
-					"width": 4,
+					"width": 2,
 					"height": 500
 				},
 				"color2": {
